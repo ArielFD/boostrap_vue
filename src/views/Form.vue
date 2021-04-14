c3d459f59a356cd3815cbd2686b073d70b245b66
arielferrera1992@gmail.com <arielferrera1992@gmail.com>
2021-04-13 20:31:44 -0400

    boostrap_vue

---
+++ src/views/Form.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="mt-5">
+    <b-row class="mt-5">
+      <b-col cols="12" md="12">
+        <h1>Formulario</h1>
+      </b-col>
+      <b-col cols="12" md="6">
+        <b-form-input
+          v-model="number"
+          type="number"
+          placeholder="Enter your number"
+          :state="comprobarNumero"
+        ></b-form-input>
+        <small>My age</small>
+        <div class="mt-2">Value: {{ number }}</div>
+      </b-col>
+      <b-col cols="12" md="6">
+        <b-form-input
+          v-model="text"
+          type="text"
+          placeholder="Enter your text"
+          :state="comprobarTexto"
+        ></b-form-input>
+        <small>My name</small>
+        <div class="mt-2">Value: {{ text }}</div>
+      </b-col>
+      <b-col class="md-3">
+          <b-form-select v-model="selected" :options="animales"></b-form-select>
+          <p>{{selected}}</p>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Form",
+  data() {
+    return {
+      text: "",
+      number: 0,
+      selected:null,
+      animales:[
+          {value:null,text: 'Please select an option'},
+          { value: 'a', text: 'This is First option' },
+          { value: 'b', text: 'This is second option' },
+          { value: 'c', text: 'This is thirds option' }
+      ]
+    };
+  },
+  computed:{
+      comprobarTexto(){
+          return this.text.match("Ariel")?true:false
+      },
+      comprobarNumero(){
+          return this.number==29?true:false
+      }
+  }
+};
+</script>
---
+++ src/views/Grid.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="mt-5">
+        <b-row>
+            <b-col cols="12" md="6" lg="4">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem harum facere dolore accusantium! Repellendus nihil, earum optio similique odit recusandae itaque totam reprehenderit aut obcaecati fugiat ad fugit, laboriosam dolores.</p>
+            </b-col>
+            <b-col cols="12" md="6" lg="8">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem harum facere dolore accusantium! Repellendus nihil, earum optio similique odit recusandae itaque totam reprehenderit aut obcaecati fugiat ad fugit, laboriosam dolores.</p>
+            </b-col>
+            <b-col cols="12" md="6" lg="4">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem harum facere dolore accusantium! Repellendus nihil, earum optio similique odit recusandae itaque totam reprehenderit aut obcaecati fugiat ad fugit, laboriosam dolores.</p>
+            </b-col>
+            <b-col cols="12" md="6" lg="8">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem harum facere dolore accusantium! Repellendus nihil, earum optio similique odit recusandae itaque totam reprehenderit aut obcaecati fugiat ad fugit, laboriosam dolores.</p>
+            </b-col>
+        </b-row>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Grid'
+}
+</script>
